b3ba92b7c4e0d8f541f70140953bd4a65c5c7734
Syed Moiz Haider <moizhaider70@gmail.com>
2026-05-25 19:06:08 +0500

    category apis implementation

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, Get, NotFoundException, Delete, Param, Patch } from '@nestjs/common';
+import { CategoryService } from './category.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+@Controller('categories')
+export class CategoryController {
+    constructor(private categoryService: CategoryService) {}
 
-@Controller('category-controller')
-export class CategoryController {}
+    @Post()
+    async create(@Body() createCategoryDto: CreateCategoryDto){
+        const category = await this.categoryService.create(createCategoryDto);
+        return {
+            message: "Category created successfully",
+            data: category
+        };
+    }
+    
+    @Get("getAll")
+    async getAll(){
+        const categories = await this.categoryService.getAll();
+        if(!categories.length) throw new NotFoundException("Categories not found");
+        return {
+            message: "Categories found",
+            data: categories
+        }
+    }
+
+    @Delete(":id")
+    async delete(@Param("id") id: number){
+        const delCategory = await this.categoryService.delete(id);
+        if(!delCategory.affected) throw new NotFoundException("Category not found.")
+        return {message: "Category deleted successfully"};
+    }
+
+    @Patch(":id")
+    async patch(@Param("id") id: number, @Body() updateCategoryDto:UpdateCategoryDto ){
+        const category = await this.categoryService.update(id, updateCategoryDto.name);
+        if(!category.affected) throw new NotFoundException("Category not found.");
+        return {
+            message: "Category updated successfully",
+            data: category
+        }
+    }
+}
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -2,11 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Category } from './category.entity';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { DeleteResult } from 'typeorm/browser';
+import { UpdateResult } from 'typeorm/browser';
 
 @Injectable()
 export class CategoryService {
-    constructor(
-        @InjectRepository(Category)
-        private categoryRepository: Repository<Category>
-    ){}
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+    const category = this.categoryRepository.create(createCategoryDto);
+    return await this.categoryRepository.save(category);
+  }
+
+  async getAll(): Promise<Category[]> {
+    return await this.categoryRepository.find({});
+  }
+
+  async delete(id: number): Promise<DeleteResult> {
+    const category: DeleteResult = await this.categoryRepository.delete({
+      id,
+    });
+    return category;
+  }
+
+  async update(id: number, name: string): Promise<UpdateResult> {
+    return await this.categoryRepository.update(id, {
+      name,
+    });
+  }
 }
